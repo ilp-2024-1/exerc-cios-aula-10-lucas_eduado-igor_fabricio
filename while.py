5015b0e8eaaef9f623e422c791e19e5f143bb5e0
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-10 11:08:14 -0300

    Junção dos arquivos em um só único

---
+++ while.py
@@ -0,0 +1,54 @@
+###1
+#n = 1
+#while n != 101:
+    #print(n)
+    #n = n + 1
+
+
+###2
+#programa = 1
+#soma = 0
+
+#print("Para encerrar o programa digite '0'. ")
+#while programa != 0:
+    #numero = int(input("Digite um número para adicionar à soma: "))
+    #programa = numero
+    #soma = soma + numero
+
+#print("O valor da soma é =", soma)
+
+
+###3
+#senha_bd = 21
+#senha_u = 0
+
+#while senha_u != 21:
+    #senha_u = int(input("Digite a senha: "))
+
+#print("Senha correta. Programa encerrado.")
+
+
+###4
+#from random import randint
+
+#numero = randint(1, 100)
+#palpite = 0
+#vezes = 0
+
+#while palpite != numero:
+    #palpite = int(input("Aposta: "))
+    #vezes = vezes + 1
+
+#print("Você precisou de",vezes,"tentativas para acertar meu número!")
+
+
+###5
+#numero = int(input("Digite um número: "))
+#fatorial = 1
+#contador = 1
+
+#while contador <= numero:
+    #fatorial *= contador
+    #contador += 1
+
+#print(fatorial)
