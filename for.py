a757a6d54f1d136c51db9e10224fd2ebfa46b307
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-07-11 08:01:15 -0300

    Exercícios 2, 3 e 4

--- for.py
+++ for.py
@@ -4,3 +4,39 @@
 # for x in range(1,13):
 #     if x % 2 == 0:
 #         print(x, end=", ")
+
+
+###2 Escreva um programa que solicite do usuário um valor positivo e
+#imprima todos os números inteiros de 1 até o número digitado pelo
+#usuário. Ao término, informe que o programa foi encerrado.
+
+# n = int(input("Digite um número positivo: "))
+# for x in range(1,n+1):
+#     print(x, end=", ")
+# print("\n Programa encerrado.")
+
+
+###3 Escreva um programa que solicite do usuário dois valores positivos
+#e imprima todos os números inteiros dentro desse intervalo. O
+#algoritmo deve armazenar cada valor em duas variáveis diferentes
+#(v_ini e v_fim). Ao término, informe que o programa foi encerrado.
+
+# v_ini = int(input("Digite o início do intervalo: "))
+# v_fim = int(input("Digite o fim do intervalo: "))
+
+# for x in range(v_ini, v_fim+1):
+#     print(x, end=", ")
+# print("\n Programa encerrado.")
+
+
+###4 Escreva um programa que solicite do usuário dois valores positivos e imprima
+#todos os números inteiros dentro desse intervalo excluindo-se o valor inicial do
+#intervalo e o valor final. Ao término, informe que o programa foi encerrado.
+
+# v_ini = int(input("Digite o início do intervalo: "))
+# v_fim = int(input("Digite o fim do intervalo: "))
+
+# for x in range(v_ini+1, v_fim):
+#     print(x, end=", ")
+# print("\n Programa encerrado.")
+
